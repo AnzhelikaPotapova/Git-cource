56dc40cb9d68f0dd9465b69dbbb30312821f16ac
Anzhelika <lika.oganesyan97@gmail.com>
2026-04-18 17:41:33 +0500

    Data changed

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,11 +1,11 @@
 public class Launcher {
     static void main(String[] args) {
+        System.out.println("Изменены вводные данные, новый результат операций:");
         Calculator Calc = new Calculator();
-        Calc.doOperation(10,4,((x, y) -> x+y));
-        Calc.doOperation(10,4,((x, y) -> x-y));
-        Calc.doOperation(10,4,((x, y) -> x*y));
-        Calc.doOperation(10,4,((x, y) -> x/y));
-
+        Calc.doOperation(15,4,((x, y) -> x+y));
+        Calc.doOperation(10,6,((x, y) -> x-y));
+        Calc.doOperation(11,4,((x, y) -> x*y));
+        Calc.doOperation(8,2,((x, y) -> x/y));
     }
 }
 @FunctionalInterface
